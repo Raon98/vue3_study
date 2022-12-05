c185e0576cbe6818794d8d27ddff9c769d7ad958
Raon98 <cheol1998@kakao.com>
2022-12-05 20:19:14 +0900

    AWS_SDK

---
+++ src/components/awsEx.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container>
+    <h1>파일업로더</h1>
+    <input id="file-selector"
+          ref="file"
+          type="file"
+           @change="handleFileUpLoad()"
+    >
+  </v-container>
+</template>
+
+<script>
+import AWS from 'aws-sdk'
+export default {
+  name: "awsEx",
+  setup() {
+
+    const handleFileUpLoad = () => {
+      console.log("파일이 업로드돼었습니다.")
+    }
+
+    return { handleFileUpLoad}
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/readMe/awsS3Bucket.md
@@ -0,0 +1,13 @@
+# AWS S3 Bucket Study
+
+- ###Amazon S3
+```vue
+- Amazon Simple Storage Service는 인터넷용 스토리지 서비스
+- Amazon S3에서 제공하는 단순한 웹 서비스 인터페이스를 사용하여 웹에서 언제 어디서나 원하는 양의 데이터를 저장하고 검색
+```
+
+- ###Amazon Cognito
+```vue
+- 웹 및 모바일 앱에 대한 인증, 권한 부여 및 사용자 관리를 제공하는 서비스
+- "사용자 풀(User Pools)" 과 "자격 증명 풀(Identity pool)"
+```
